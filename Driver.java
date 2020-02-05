b91d871268ce4047e1dc63f64ad7fd9ec5d62b28
Carter Bogie <william.c.bogie-1@ou.edu>
2020-02-05 14:40:02 -0600

    Initial commit

---
+++ Driver.java
@@ -0,0 +1,8 @@
+/*
+ * This program creates an elementary cellular automata and displays it in an easy
+ * to understand visual way. This class is the driver class that conatins the main()
+ * method and is not supposed to be 
+ */
+public class Driver {
+
+}
